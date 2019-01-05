396fcdb47c310e1158cbf6f76b8e6caec50c279f
Conor Flannery <cflannery91@gmail.com>
2019-01-05 13:26:09 +0000

    New Tests

--- WebApiOauthDemo.Tests/TestHomeController.cs
+++ WebApiOauthDemo.Tests/TestHomeController.cs
@@ -19,13 +19,13 @@ namespace WebApiOauthDemo.Tests
             Assert.AreNotEqual(6, home.Add(x, y));
         }
 
-        //[TestMethod]
-        //public void TestAddFail()
-        //{
-        //    int x = 5;
-        //    int y = 2;
+        [TestMethod]
+        public void TestAddFail()
+        {
+            int x = 5;
+            int y = 2;
 
-        //    Assert.AreEqual(6, home.Add(x, y));
-        //}
+            Assert.AreEqual(6, home.Add(x, y));
+        }
     }
 }
